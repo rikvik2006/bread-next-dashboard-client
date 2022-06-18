4080cebfbf93bad4a94bda7190b8065d9a16f58f
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-06-18 15:00:17 +0200

    now not authorized users cant go in menu page

---
+++ src/utils/api.ts
@@ -0,0 +1,19 @@
+import { GetServerSidePropsContext } from "next";
+import axios from "axios";
+import { validateCookies } from "./helpers";
+import { Guild } from "./types";
+
+const API_URL = "http://localhost:3001/api"
+
+export const FetchMutalGuilds = async (context: GetServerSidePropsContext) => {
+    const headers = validateCookies(context);
+    if (!headers) return { redirect: { destination: "/" } };
+
+    try {
+        const { data: guilds } = await axios.get<Guild[]>(`${API_URL}/guilds`, { headers });
+        return { props: { guilds } };
+    } catch (err) {
+        console.log(err);
+        return { redirect: { destination: "/" } };
+    }
+};
---
+++ src/utils/types.ts
@@ -0,0 +1,8 @@
+export type Guild = {
+    id: string;
+    name: string;
+    icon: string;
+    owner: string;
+    permissions: string;
+    features: string[];
+};
