91b634c0a716880e0189597cdf78a149f468908c
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2022-06-20 01:09:20 +0200

    Set up a router for appbar

--- src/components/misc/Appbar.tsx
+++ src/components/misc/Appbar.tsx
@@ -1,10 +1,13 @@
 import styles from "./index.module.scss";
 import { RiMenu3Line } from "react-icons/ri";
+import { useRouter } from "next/router";
 
 export const Appbar = () => {
+    const router = useRouter()
+
     return (
         <div className={styles.appbar}>
-            <div className={styles.menu}>
+            <div className={styles.menu} onClick={() => router.push("/menu")}>
                 <RiMenu3Line size={65} />
                 <p>Menu</p>
             </div>
